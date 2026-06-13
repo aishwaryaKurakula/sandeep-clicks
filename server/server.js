85985c396e9dcc3edd78f0e0645ec2e805d9f39c
aishwaryakurakula2511@gmail.com <aishwaryakurakula2511@gmail.com>
2026-06-13 17:25:07 +0530

    added db connection in db.js

--- server/server.js
+++ server/server.js
@@ -10,11 +10,8 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-    console.log('MongoDB connected');
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-}).catch((err) => {
-    console.log('MongoDB connection error:', err);
-});
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+})
