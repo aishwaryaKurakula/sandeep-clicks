bd37629c781783b39a4c8d4fc17a0c0bd99d059a
aishwaryakurakula2511@gmail.com <aishwaryakurakula2511@gmail.com>
2026-06-14 22:26:02 +0530

    production issues

--- client/src/api/gallery.js
+++ client/src/api/gallery.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:5000/api/gallery'
+const BASE_URL = 'https://sandeep-clicks.onrender.com/api/gallery'
 
 // fetch all photos — used on gallery page
 export const getAllPhotos = async () => {
